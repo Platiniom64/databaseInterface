d6daaa8a1d23c737d818b2943898aaaffee15eb7
Vladimir Hanin <vladimir.hanin@outlook.com>
2020-07-09 14:36:09 +0200

    added the three tabs where one is for the info, one is for adding data and the last tab is for retreiveing data

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 import tkinter as tk
+from tkinter import ttk
 
-# set up of the window
-window = tk.Tk()
-window.geometry("500x500")
-window.title("user interface database")
+# * set up of the root
+root = tk.Tk()
+root.geometry("500x500")
+root.title("user interface database")
 
-# set up the text in the window
-titleLabel = tk.Label(window, text="Enter new data into database here:", font="bold")
+# * creation of the different tabs of the window
+rootTab = ttk.Notebook(root)
+
+tab1 = ttk.Frame(rootTab)
+rootTab.add(tab1, text="info")
+
+tab2 = ttk.Frame(rootTab)
+rootTab.add(tab2, text="add data")
+
+tab3 = ttk.Frame(rootTab)
+rootTab.add(tab3, text="retrieve data")
+
+rootTab.pack(fill="both")
+
+# * set up of the first tab
+titleLabel = tk.Label(tab1, text="Information about the program", font="bold")
 titleLabel.pack()
 
-# opens the window
-window.mainloop()
+textLabel = tk.Label(tab1, text="This progam .. BLA BLA BLA EDIT HERE")
+textLabel.pack()
+
+# * set up of the second tab
+titleLabel = tk.Label(tab2, text="Enter new data into database", font="bold")
+titleLabel.pack()
+
+# * set up of the third tab
+titleLabel = tk.Label(tab3, text="retreive data from database", font="bold")
+titleLabel.pack()
+
+
+# opens the root
+root.mainloop()
